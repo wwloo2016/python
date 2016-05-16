d1f38440fb3a1a206e30a767d23dc12adad80863
sunjingfang923 <sunjingfang923@gmail.com>
2016-05-16 22:07:51 +0800

    first commit

---
+++ backup/backup.py
@@ -0,0 +1,35 @@
+#!/root/.pyenv/versions/backp/bin/python
+#Filename:backup.sh
+
+import os,time
+
+source = '/root/python'
+
+target_dir = '/tmp/backup/'
+
+today = target_dir + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+comment = input('Enter a comment -->')
+if len(comment) == 0:
+    target = today + os.sep + os.path.basename(source) + now + '.zip'
+else:
+    target = today + os.sep + os.path.basename(source) + now + '_' + comment.replace(' ','_') + '.zip'
+# try:
+#     if not os._exists(today):
+#         os.mkdir(today)
+#         print('Successed created directory',today)
+# except FileExistsError as e:
+#     print('Warning:{0}'.format(e))
+# finally:
+#     pass
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successed created directory',today)
+zip_command = "zip -qr %s %s" % (target,''.join(source))
+
+if os.system(zip_command) == 0:
+    print('Successful backup to ',target)
+else:
+    print('Backup FAILED')
+
+
